7f04667b40b5758389970495ea604ac9ded38331
Ujoonnee <dnwns0933@naver.com>
2022-03-18 10:44:27 +0900

    project init

---
+++ MidProject/src/main/java/co/yedam/MidProject/common/Command.java
@@ -0,0 +1,8 @@
+package co.yedam.MidProject.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Command {
+	String execute(HttpServletRequest request, HttpServletResponse response);
+}
---
+++ MidProject/src/main/java/co/yedam/MidProject/common/DataSource.java
@@ -0,0 +1,27 @@
+package co.yedam.MidProject.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class DataSource {
+	private static SqlSessionFactory sqlSessionFactory;
+	private DataSource() {}
+	
+	public static SqlSessionFactory getInstance() {
+		// sqlSessionFactory 생성 시 가져올 환경설정의 위치
+		String resource = "config/mybatis-config.xml";
+		try {
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return sqlSessionFactory;
+	}
+}
